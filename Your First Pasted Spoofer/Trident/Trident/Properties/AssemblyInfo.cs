b0904ab51f2ee7f82b15948378204c75addd40ff
hugzho <61623190+hugzho@users.noreply.github.com>
2020-03-11 14:01:57 -0400

    Add files via upload

---
+++ Your First Pasted Spoofer/Trident/Trident/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyTitle("CripSpoofer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CripSpoofer")]
+[assembly: AssemblyProduct("CripSpoofer")]
+[assembly: AssemblyCopyright("Copyright © CripSpoofer")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("042b6a37-2c4b-4cae-b2fc-8d10a908541f")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
